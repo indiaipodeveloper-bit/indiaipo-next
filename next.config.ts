0b62ced2b14a8fa9734c1d3dc442e2bf3aa99865
IPO <indiaipodeveloper@gmail.com>
2026-06-23 10:16:30 +0530

    update next.config.ts to add prod api url

--- next.config.ts
+++ next.config.ts
@@ -9,13 +9,13 @@ const nextConfig: NextConfig = {
       {
         source: "/api/:path*",
         destination: process.env.NODE_ENV === "production"
-          ? "https://www.indiaipo.in/api/:path*"
+          ? "https://api.indiaipo.in/api/:path*"
           : "http://localhost:5000/api/:path*",
       },
       {
         source: "/uploads/:path*",
         destination: process.env.NODE_ENV === "production"
-          ? "https://www.indiaipo.in/uploads/:path*"
+          ? "https://api.indiaipo.in/uploads/:path*"
           : "http://localhost:5000/uploads/:path*",
       },
     ];
